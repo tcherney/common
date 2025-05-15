72167339c5ee514da7eb99bc901ea5b42d58cb4e
Timothy Cherney <tjcherney@hotmail.com>
2025-05-15 15:40:10 -0700

    more colors

--- src/colors.zig
+++ src/colors.zig
@@ -9,5 +9,12 @@ pub const Colors = struct {
     pub const YELLOW = Pixel.init(255, 255, 0, null);
     pub const MAGENTA = Pixel.init(255, 0, 255, null);
     pub const CYAN = Pixel.init(0, 255, 255, null);
+    pub const SLATE = Pixel.init(112, 128, 144, null);
+    pub const GRAY = Pixel.init(128, 128, 128, null);
+    pub const STONE = Pixel.init(146, 142, 133, null);
+    pub const BROWN = Pixel.init(51, 24, 0, null);
+    pub const WENGE = Pixel.init(100, 84, 82, null);
+    pub const DARK_BROWN = Pixel.init(92, 64, 51, null);
+    pub const BEAVER = Pixel.init(159, 129, 112, null);
     //TODO add more colors
 };
