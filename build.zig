7ab69bbacd770a4e4d65acd775f2140e10733254
Timothy Cherney <tjcherney@hotmail.com>
2026-04-10 01:27:45 -0700

    doc gen for common

--- build.zig
+++ build.zig
@@ -49,4 +49,12 @@ pub fn build(b: *std.Build) void {
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
+    //TODO make sure this works
+    const install_docs = b.addInstallDirectory(.{
+        .source_dir = lib.getEmittedDocs(),
+        .install_dir = .prefix,
+        .install_subdir = "docs",
+    });
+    const docs_step = b.step("docs", "Generate documentation");
+    docs_step.dependOn(&install_docs.step);
 }
